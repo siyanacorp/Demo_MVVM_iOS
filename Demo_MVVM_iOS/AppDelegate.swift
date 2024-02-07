59a3a5192b5374b9192a18e7129977622decab2c
Ritik Tyagi <tyagiritik339@gmail.com>
2024-02-07 13:14:18 +0530

    Scene delegate documentation done, App delegate documentations updated.

--- Demo_MVVM_iOS/AppDelegate.swift
+++ Demo_MVVM_iOS/AppDelegate.swift
@@ -12,34 +12,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     /**
      Application did finish launching with options
+     * Override point for customization after application launch
+     
      - Parameter application: Main application reference
      - Parameter launchOptions: Application launch options Keys
      - Returns: A boolean value (true/false) to tell if application is launched successfully with options. */
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
     
     /**
      Application configuration for connecting
+     * Called when a new scene session is being created.
+     * Use this method to select a configuration to create the new scene with.
+     
      - Parameter application: Main application reference
      - Parameter connectingSceneSession: UIScene session
      - Parameter options: UIScene connection options
      - Returns: UIScene configuration  */
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
     /**
      Application did discard scene sessions
+     * Called when the user discards a scene session.
+     * If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+     * Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+     
      - Parameter application: Main application reference
      - Parameter sceneSessions: Set UIScene session */
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
     
--- Demo_MVVM_iOS/SceneDelegate.swift
+++ Demo_MVVM_iOS/SceneDelegate.swift
@@ -7,46 +7,86 @@
 
 import UIKit
 
+/// The Scene Delegate is a crucial component introduced in iOS 13 to manage the lifecycle of multiple scenes in an iOS app, including windows and views. It acts as a coordinator for interactions between the app and the system, allowing for the creation, modification, and destruction of scenes as needed. Here's a detailed description of the Scene Delegate:
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
+    /// In the context of iOS development, the "window" variable typically refers to the UIWindow object, which serves as the root view container for an iOS application. Here's a description of the window variable in an iOS app:
     var window: UIWindow?
-
-
+    
+    /**
+     Scene will connect to
+     
+     * Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+     * If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+     * This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+     
+     - Parameter scene: UIScene
+     - Parameter session: UIScene session
+     - Parameter connectionOptions: UIScene connection options
+     */
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
+    /**
+     Scene did disconnect
+     * Called as the scene is being released by the system.
+     * This occurs shortly after the scene enters the background, or when its session is discarded.
+     * Release any resources associated with this scene that can be re-created the next time the scene connects.
+     * The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+     
+     - Parameter scene: UIScene
+     */
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        
     }
-
+    
+    /**
+     Scene did become active
+     * Called when the scene has moved from an inactive state to an active state.
+     * Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+     
+     - Parameter scene: UIScene
+     */
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
     }
-
+    
+    /**
+     Scene will resign active
+     * Called when the scene will move from an active state to an inactive state.
+     * This may occur due to temporary interruptions (ex. an incoming phone call).
+     
+     - Parameter scene: UIScene
+     */
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        
     }
-
+    
+    /**
+     Scene will enter foreground
+     * Called as the scene transitions from the background to the foreground.
+     * Use this method to undo the changes made on entering the background.
+     
+     - Parameter scene: UIScene
+     */
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        
     }
-
+    
+    /**
+     Scene did enter background
+     * Called as the scene transitions from the foreground to the background.
+     * Use this method to save data, release shared resources, and store enough scene-specific state information
+     * to restore the scene back to its current state.
+     
+     - Parameter scene: UIScene
+     */
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        
     }
-
-
+    
+    
 }
 
