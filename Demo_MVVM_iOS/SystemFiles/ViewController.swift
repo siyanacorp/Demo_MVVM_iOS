02f668bf4b027aa5ad142bd4e13da76165c24a77
siyanacorp <contact.siyanacorp@gmail.com>
2024-02-07 13:24:43 +0530

    view controller file commenting done

--- Demo_MVVM_iOS/SystemFiles/ViewController.swift
+++ Demo_MVVM_iOS/SystemFiles/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOUTLETS
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - VIEW LIFE CYCLE METHODS
+    // TODO: VIEW DID LOAD METHOD
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // TODO: DEINIT
+    deinit{
+        print("ViewController has been REMOVED...!")
+    }
+    
+    // MARK: - ACTIONS
 }
 
